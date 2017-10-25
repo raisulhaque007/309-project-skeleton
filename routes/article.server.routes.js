f963abbe8674a6687c763f0ca426ee65ad7eead9
Raisul Haque <rhrdinulhaqq@gmail.com>
2017-10-25 03:32:12 +0000

    Assignment 10

--- routes/article.server.routes.js
+++ routes/article.server.routes.js
@@ -3,20 +3,27 @@ module.exports = function(app){
  var articles = require('./../controllers/articles.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
 
- app.route('/api/articles')
+ app.route('/articles/new')
+	.get(articles.new);
+
+
+  app.route('/articles/all')
+	.get(articles.all);
+  
+	app.route('/articles/:articleId')
+	.get(articles.view);
+
+	app.route('/articles/edit/articleId')
+	.get(articles.edit);
+
+
+	app.route('api/articles')
 	.get(articles.list)
-	.post(users.requiresLogin, articles.create);
-
-  app.route('/api/articles/:articleId')
-	.get(articles.read)
-  .delete(users.requiresLogin, articles.delete);
-
-	app.route('/api/articles/edit/:articleId')
-	.get(articles.read)
-	.put(users.requiresLogin, articles.update);
-
-
-app.param('articleId', articles.articleByID);
-
+	.post(users.requestLogin,articles.create);
+	
+//	app.route('api/articles/:articleID')
+//	.get(articles.read)
+//	.delete(users.requestLogin,articles.delete);
+	
 
 }
